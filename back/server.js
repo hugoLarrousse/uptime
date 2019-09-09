efce96e3840b4201a277162da761aa1e779c84f6
Hugo Larrousse <hugo.larrousse@gmail.com>
2019-09-09 18:51:25 +0200

    initialization uptime-core

--- back/server.js
+++ back/server.js
@@ -1,3 +1,12 @@
 const express = require('express')();
 
 const server = require('http').createServer(express);
+
+
+express.all('/', (req, res) => {
+  res.status(200).send({ message: 'Welcome to uptime' });
+});
+
+server.listen(3002, () => {
+  console.log(`Uptime-core is running at ${new Date()} on 3002`);
+});
